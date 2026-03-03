d9bf91f21e5e01c449ef39c5660c05e3e5cdcd8e
FatOrangeApple <Mikey2possible@gmail.com>
2026-03-03 12:21:45 +1100

    Input Change
    Changed the input to a getline & stringstream parser

--- calculator.cpp
+++ calculator.cpp
@@ -2,64 +2,83 @@
 #include <iomanip>
 #include <limits>
 #include <string>
+#include <sstream>
 
-double add(double a, double b) {
-    return a + b;
+double add(double a, double b){
+    return (a + b);
 }
 
-double subtract(double a, double b) {
-    return a - b;
+double subtract(double a, double b){
+    return (a - b);
 }
 
-double multiply(double a, double b) {
-    return a * b;
+double multiply(double a, double b){
+    return (a * b);
 }
 
-double divide(double a, double b) {
-    return a / b;
+double divide(double a, double b){
+    return (a / b);
 }
 
-int main() {
-    std::cout << std::fixed << std:: setprecision(2);
 
-    double num1, num2;
-    char op;
-    std::string term;
+double num1, num2;
+char op;
 
-    std::cout << "Welcome to Regans online Calc\n" 
-    << "Remember! Calc stands for calculator\n" 
-    << "Enter your operation:" << std::endl;
+int main(){
 
-    while (term != "end"){
+    std::cout << std::fixed << std::setprecision(2);
     
-    std::cin >> num1 >> op >> num2;
+    std::cout << "Welcome to Regan's elementary school calculator\n" 
+    << "Type 'end' to terminate the program\n" 
+    << "Please enter your equation" << std::endl;
 
-    switch (op) {
-        case '+':
-            std::cout << add(num1, num2) << std::endl;
-            break;
+    while (true){
 
-        case '-':
-            std::cout << subtract(num1, num2) << std::endl;
-            break;
-        
-        case '*':
-        case 'x': 
-            std::cout << multiply(num1, num2) << std::endl;
-            break;
+        std::string line;
 
-        case '/':
-            std::cout << divide(num1, num2) << std::endl;
-            break;
-        
-        default:
-            std::cout << "Maths only bud, try again." << std::endl;
-            break;
+        std::getline(std::cin, line);
+        std::cout << std::endl;
+
+        if (line == "end") break;
+
+        std::stringstream ss(line);
+        ss >> num1 >> op >> num2;
+
+        if (ss.fail()) {
+            std::cout << "Only add numbers bud this isn't highschool" << std::endl;
+            continue;
+        }
+
+        switch (op) {
+
+            case '+':
+                std::cout << add(num1, num2) << "\n" << std::endl;
+                break;
+
+            case '-':
+                std::cout << subtract(num1, num2) << "\n" << std::endl;
+                break;
+            
+            case 'x':
+            case '*':
+                std::cout << multiply(num1, num2) << "\n" << std::endl;
+                break;
+            
+            case '/':
+                if (num2 != 0) {
+                    std::cout << divide(num1, num2) << "\n" << std::endl;
+                }
+                else {
+                    std::cout << "What are you, Epstein?" << "\n" << std::endl;
+                }
+                break;
+
+            default:
+                std::cout << "Simple operations only bud, I ain't Einstein" << "\n" << std::endl;
+                break;
+
+        }
+
     }
-
-    std::cout << "Would you like to terminate the program?" << std::endl;
-    std::cin >> term;
-    }
-
-    return 0;
+    std::cout << "Remember,\n" << "Calc stands for calculator" << std::endl;
 }
